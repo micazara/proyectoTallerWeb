ad4cae99a3c4f84950b7cc0f009c6a2f2e9e09c7
LudmiAbril <ludmila.pereyra543@gmail.com>
2023-10-01 15:39:15 -0300

    odio este ide

---
+++ src/main/java/com/tallerwebi/presentacion/ControladorFase3.java
@@ -0,0 +1,11 @@
+package com.tallerwebi.presentacion;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ControladorFase3 {
+    @RequestMapping(path = "/fase03")
+    public ModelAndView fase03() {
+        return new ModelAndView("fase03");
+    }
+}
