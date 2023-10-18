a96410cc22ff5bbefed399031bf9f4c1d76daf14
LudmiAbril <ludmila.pereyra543@gmail.com>
2023-10-18 12:15:35 -0300

    entidad nivel

---
+++ src/main/java/com/tallerwebi/dominio/Nivel.java
@@ -0,0 +1,33 @@
+package com.tallerwebi.dominio;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombrel;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombreNivel) {
+        this.nombre = nombreNivel;
+    }
+   
+}
+
+
+
+
