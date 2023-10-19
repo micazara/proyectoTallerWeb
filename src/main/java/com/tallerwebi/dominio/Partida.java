77c459c1b7da0aea217f2a86b38eb7f477c8f56e
Celena Moscovich <celena.ark@gmail.com>
2023-10-19 16:56:04 -0300

    vista 03

--- src/main/java/com/tallerwebi/dominio/Partida.java
+++ src/main/java/com/tallerwebi/dominio/Partida.java
@@ -1,13 +1,18 @@
 package com.tallerwebi.dominio;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
 @Entity
 public class Partida {
 
 	// TIENE UN ID DE LA PARTIDA,EL USUARIO,NUMERO DEL NIVEL
-
+	
+	 @Id
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private Integer vidas;
 	private Nivel nivel;
